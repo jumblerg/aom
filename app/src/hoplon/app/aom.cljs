ea19025dc16f84f7752b525ab1736bf9cb1b44fc
jumblerg <jumblerg@gmail.com>
2017-01-11 09:00:40 -0500

    piles stack elements

--- app/src/hoplon/app/aom.cljs
+++ app/src/hoplon/app/aom.cljs
@@ -8,17 +8,17 @@
     [hoplon.aom.attrs :refer [s sh sv]]
     [hoplon.aom.elems :refer [flow pile]]))
 
-(hoplon.core/html
+(hoplon.core/html :css/height "100%"
   (hoplon.core/head
     (hoplon.core/title "hello"))
-  (hoplon.core/body :css/margin 0
+  (hoplon.core/body :css/height "100%" :css/margin 0
     (flow s (% 1 1)
-      (pile sh (% 1 2)
+      (pile sh (% 1 2) sv (% 1 1)
         (flow s (% 1 1)
           "foo")
         (flow s (% 1 1)
           "bar"))
-      (pile sh (% 1 2)
+      (pile sh (% 1 2) sv (% 1 1)
         (flow s (% 1 1)
           "baz")
         (flow s (% 1 1)
