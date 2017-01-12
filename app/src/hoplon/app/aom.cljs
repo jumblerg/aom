8efa38a5d3bc737c3cce1dee355a0331f95c4e4e
jumblerg <jumblerg@gmail.com>
2017-01-12 16:39:11 -0500

    add colors

--- app/src/hoplon/app/aom.cljs
+++ app/src/hoplon/app/aom.cljs
@@ -4,21 +4,22 @@
   (:require
     [javelin.core :refer [cell cell= defc defc=]]
     [hoplon.core  :refer [defelem for-tpl when-tpl case-tpl]]
-    [hoplon.aom.units :refer [%]]
-    [hoplon.aom.attrs :refer [s sh sv]]
-    [hoplon.aom.elems :refer [flow pile]]))
+    [hoplon.aom.units  :refer [%]]
+    [hoplon.aom.colors :refer [hsl rgb]]
+    [hoplon.aom.attrs  :refer [s sh sv fc]]
+    [hoplon.aom.elems  :refer [flow pile]]))
 
 (hoplon.core/html :css/height "100%"
   (hoplon.core/head
     (hoplon.core/title "hello"))
   (hoplon.core/body :css/height "100%" :css/margin 0
     (flow s (% 1 1)
-      (pile sh (% 1 2) sv (% 1 1)
+      (pile sh (% 1 2) sv (% 1 1) fc (hsl 0 (% 1 3) (% 1 2))
         (flow s (% 1 1)
           "foo")
         (flow s (% 1 1)
           "bar"))
-      (pile sh (% 1 2) sv (% 1 1)
+      (pile sh (% 1 2) sv (% 1 1) fc (hsl 270 (% 1 3) (% 1 2))
         (flow s (% 1 1)
           "baz")
         (flow s (% 1 1)
